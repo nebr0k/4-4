0eaaac745b06c343fc605e27740a05c0de7d6a00
nebr0k <90468237+nebr0k@users.noreply.github.com>
2021-10-19 15:16:43 +0300

    Add files via upload

---
+++ 4.4.cpp
@@ -0,0 +1,38 @@
+﻿//Kоцюба Остап в-4
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, R, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "R = "; cin >> R;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << setw(4) << " |"
+		<< setw(6) << "y" << setw(6)  << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x <= 0)
+			y = -(R * x + 6 * R) / 6;
+		else
+			if (x <= R)
+				y = -sqrt(R * R - x * x);
+			else
+				if (x > R && x < 2 * R)
+					y = sqrt(R * R - (x - R) * (x - R));
+				else
+					y = R;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
